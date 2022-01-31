a28c4de46a8b0822a663a2d39e6a475b2c9f06b0
Saburov <the.saburchik@gmail.com>
2022-01-31 17:50:02 +0700

    added method .map for arrays

--- src/components/Main/Dialogs/Dialogs.jsx
+++ src/components/Main/Dialogs/Dialogs.jsx
@@ -5,6 +5,20 @@ import Dialog from './Dialog/Dialog'
 import Message from './Message/Message'
 
 const Dialogs = (props) => {
+
+    const dialogs = [
+        { id: 1, name: 'Andrey' },
+        { id: 3, name: 'Masha' },
+        { id: 4, name: 'David' }
+    ]
+    const messages = [
+        { id: 1, message: 'Hello, how are you?' },
+        { id: 4, message: 'lorem somethin text about blah-blah' }
+    ]
+
+    const dialogsDraw = dialogs.map(dia => <Dialog name={dia.name} id={dia.id} />)
+    const messagesDraw = messages.map(mes => <Message name={mes.name} id={mes.id} message={mes.message} />)
+
     return (
         <section className='dialogs'>
             <h2>Dialogs</h2>
@@ -12,15 +26,12 @@ const Dialogs = (props) => {
                 <section className='dialog__lists'>
                     <h3>Messaging</h3>
                     <ul className='list__items'>
-                        <Dialog name='Andrey' id='1' />
-                        <Dialog name='Masha' id='2' />
-                        <Dialog name='David' id='3' />
+                        {dialogsDraw}
                     </ul>
                 </section>
                 <div className='dialog__messages'>
                     <ul className='message__items'>
-                        <Message name='Me' id='11' message='Hello, how are you?' />
-                        <Message name='David' id='3' message='lorem somethin text about blah-blah' />
+                        {messagesDraw}
                     </ul>
                     <div className='message__sends'>
                         <textarea className='message__textarea' placeholder='Write a message...' />
--- src/components/Main/Profile/MyPosts/MyPosts.jsx
+++ src/components/Main/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,15 @@ import './MyPosts.scss'
 import Post from './Post/Post'
 
 const Posts = () => {
+
+    const posts = [
+        { id: 1, message: 'Hello! How are you man?' },
+        { id: 2, message: 'Oh my god, who I looking at? I am fine bro' },
+        { id: 3, message: '' }
+    ]
+
+    const postsDraw = posts.map(pos => <Post id={pos.id} message={pos.message} />)
+
     return (
         <article className='posts__inner'>
             <h3 className='title-h3'>My posts</h3>
@@ -12,9 +21,7 @@ const Posts = () => {
                 <button>Add post</button>
             </div>
             <aside className='posts__wrapper'>
-                <Post message='Hello! How are you man?' />
-                <Post message='Oh my god, who I looking at? I am fine bro' />
-                <Post />
+                {postsDraw}
             </aside>
         </article>
     )
