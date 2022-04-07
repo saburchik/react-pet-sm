a76da5a3f19c69da505ed80af5731ef6f4667fb7
Saburov <the.saburchik@gmail.com>
2022-04-07 23:57:45 +0700

    added some styles and settings setUsers

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -3,7 +3,23 @@ import React from "react"
 // == Styles:
 import './Users.scss'
 
-const Users = ({ users }) => {
+const Users = ({ users, follow, unfollow, setUsers }) => {
+
+    if (users.length === 0) {
+        setUsers([
+            {
+                id: 1,
+                fullName: 'Maksim',
+                status: 'I am boss',
+                location: { country: 'Russian', city: 'Novosibirsk' },
+                avatar: 'https://avatars.mds.yandex.net/i?id=a21db4645e590d90b937f39f94686372-5648147-images-thumbs&n=13&exp=1',
+                followed: false
+            },
+            { id: 2, fullName: 'Jack', status: 'I am not boss', location: { country: 'Estonia', city: 'Narva' }, avatar: 'https://avatars.mds.yandex.net/i?id=97ab89d5b25d982ea3f7c1836b8a0359-5477525-images-thumbs&n=13&exp=1', followed: false },
+            { id: 3, fullName: 'Stephan', status: 'I am boss too', location: { country: 'Ukraine', city: 'Kiev' }, avatar: 'https://avatars.mds.yandex.net/i?id=e02adda79ec9e80ed7ce0220937d21f1-5450792-images-thumbs&n=13&exp=1', followed: true },
+            { id: 4, fullName: 'Nicole', status: 'I am director', location: { country: 'Latvia', city: 'Riga' }, avatar: 'https://avatars.mds.yandex.net/i?id=71b183a5367053a01e88f9d02e95a048-4826597-images-thumbs&n=13&exp=1', followed: true }
+        ])
+    }
 
     return (
         <div className="users">
@@ -14,13 +30,18 @@ const Users = ({ users }) => {
                         <li className="user__item" key={u.id}>
                             <div className="user__photo">
                                 <img src={u.avatar} alt="" />
-                                <button>{u.followed === false ? 'Unfollow' : 'Follow'}</button>
+                                {u.followed
+                                    ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
+                                    : <button onClick={() => follow(u.id)}>Follow</button>
+
+
+                                }
                             </div>
                             <div className="user__info">
-                                <p>{u.fullName}</p>
-                                <p>{u.status}</p>
-                                <p>{u.location.country}</p>
-                                <p>{u.location.city}</p>
+                                <p className="user__info-name">{u.fullName}</p>
+                                <p className="user__info-country">{u.location.country}</p>
+                                <p className="user__info-status">{u.status}</p>
+                                <p className="user__info-city">{u.location.city}</p>
                             </div>
                         </li>
                     )
--- src/redux/reducers/users-reducer.js
+++ src/redux/reducers/users-reducer.js
@@ -4,13 +4,13 @@ const SET_USERS = 'SET-USERS'
 
 const data = {
     users: [
-        {
-            id: 1, fullName: 'Maksim', status: 'I am boss', location: { country: 'Russian', city: 'Novosibirsk' }, avatar: 'https://avatars.mds.yandex.net/i?id=a21db4645e590d90b937f39f94686372-5648147-images-thumbs&n=13&exp=1'
-            , followed: true
-        },
-        { id: 2, fullName: 'Jack', status: 'I am not boss', location: { country: 'Estonia', city: 'Narva' }, avatar: 'https://avatars.mds.yandex.net/i?id=97ab89d5b25d982ea3f7c1836b8a0359-5477525-images-thumbs&n=13&exp=1', followed: true },
-        { id: 3, fullName: 'Stephan', status: 'I am boss too', location: { country: 'Ukraine', city: 'Kiev' }, avatar: 'https://avatars.mds.yandex.net/i?id=e02adda79ec9e80ed7ce0220937d21f1-5450792-images-thumbs&n=13&exp=1', followed: false },
-        { id: 4, fullName: 'Nicole', status: 'I am director', location: { country: 'Latvia', city: 'Riga' }, avatar: 'https://avatars.mds.yandex.net/i?id=71b183a5367053a01e88f9d02e95a048-4826597-images-thumbs&n=13&exp=1', followed: false }
+        // {
+        //     id: 1, fullName: 'Maksim', status: 'I am boss', location: { country: 'Russian', city: 'Novosibirsk' }, avatar: 'https://avatars.mds.yandex.net/i?id=a21db4645e590d90b937f39f94686372-5648147-images-thumbs&n=13&exp=1'
+        //     , followed: false
+        // },
+        // { id: 2, fullName: 'Jack', status: 'I am not boss', location: { country: 'Estonia', city: 'Narva' }, avatar: 'https://avatars.mds.yandex.net/i?id=97ab89d5b25d982ea3f7c1836b8a0359-5477525-images-thumbs&n=13&exp=1', followed: false },
+        // { id: 3, fullName: 'Stephan', status: 'I am boss too', location: { country: 'Ukraine', city: 'Kiev' }, avatar: 'https://avatars.mds.yandex.net/i?id=e02adda79ec9e80ed7ce0220937d21f1-5450792-images-thumbs&n=13&exp=1', followed: true },
+        // { id: 4, fullName: 'Nicole', status: 'I am director', location: { country: 'Latvia', city: 'Riga' }, avatar: 'https://avatars.mds.yandex.net/i?id=71b183a5367053a01e88f9d02e95a048-4826597-images-thumbs&n=13&exp=1', followed: true }
     ]
 }
 
@@ -18,7 +18,7 @@ const usersReducer = (state = data, action) => {
 
     switch (action.type) {
         case SET_USERS: {
-            return { ...state, users: [...state.users, action.users] }
+            return { ...state, users: [...state.users, ...action.users] }
         }
         case FOLLOW:
             return {
