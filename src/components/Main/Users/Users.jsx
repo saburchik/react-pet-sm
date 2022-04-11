e858abd0e61124186da50f7aad1a1f6c94cf1af5
Saburov <the.saburchik@gmail.com>
2022-04-11 23:55:18 +0700

    added axios for api

--- src/components/Main/Users/Users.jsx
+++ src/components/Main/Users/Users.jsx
@@ -1,4 +1,5 @@
 // == Base:
+import * as axios from "axios"
 import React from "react"
 // == Styles:
 import './Users.scss'
@@ -6,19 +7,11 @@ import './Users.scss'
 const Users = ({ users, follow, unfollow, setUsers }) => {
 
     if (users.length === 0) {
-        setUsers([
-            {
-                id: 1,
-                fullName: 'Maksim',
-                status: 'I am boss',
-                location: { country: 'Russian', city: 'Novosibirsk' },
-                avatar: 'https://avatars.mds.yandex.net/i?id=a21db4645e590d90b937f39f94686372-5648147-images-thumbs&n=13&exp=1',
-                followed: false
-            },
-            { id: 2, fullName: 'Jack', status: 'I am not boss', location: { country: 'Estonia', city: 'Narva' }, avatar: 'https://avatars.mds.yandex.net/i?id=97ab89d5b25d982ea3f7c1836b8a0359-5477525-images-thumbs&n=13&exp=1', followed: false },
-            { id: 3, fullName: 'Stephan', status: 'I am boss too', location: { country: 'Ukraine', city: 'Kiev' }, avatar: 'https://avatars.mds.yandex.net/i?id=e02adda79ec9e80ed7ce0220937d21f1-5450792-images-thumbs&n=13&exp=1', followed: true },
-            { id: 4, fullName: 'Nicole', status: 'I am director', location: { country: 'Latvia', city: 'Riga' }, avatar: 'https://avatars.mds.yandex.net/i?id=71b183a5367053a01e88f9d02e95a048-4826597-images-thumbs&n=13&exp=1', followed: true }
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(res => {
+                debugger
+                setUsers(res.data.items)
+            })
     }
 
     return (
@@ -29,19 +22,21 @@ const Users = ({ users, follow, unfollow, setUsers }) => {
                     return (
                         <li className="user__item" key={u.id}>
                             <div className="user__photo">
-                                <img src={u.avatar} alt="" />
-                                {u.followed
-                                    ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
-                                    : <button onClick={() => follow(u.id)}>Follow</button>
-
-
+                                <img src={
+                                    u.photos.large !== null
+                                        ? u.photos.large
+                                        : 'https://avatars.mds.yandex.net/i?id=3bdd061bff68b2ae3e738956432bb77b-2399446-images-thumbs&n=13&exp=1'} alt="" />
+                                {
+                                    u.followed
+                                        ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
+                                        : <button onClick={() => follow(u.id)}>Follow</button>
                                 }
                             </div>
                             <div className="user__info">
-                                <p className="user__info-name">{u.fullName}</p>
-                                <p className="user__info-country">{u.location.country}</p>
+                                <p className="user__info-name">{u.name}</p>
+                                {/* <p className="user__info-country">{u.location.country}</p> */}
                                 <p className="user__info-status">{u.status}</p>
-                                <p className="user__info-city">{u.location.city}</p>
+                                {/* <p className="user__info-city">{u.location.city}</p> */}
                             </div>
                         </li>
                     )
